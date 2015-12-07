1d68a092e65b1d4bdf5a3abb18a4e0865c050444
Luke Larsen <luke@lukelarsen.com>
2015-12-07 15:47:41 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+[PostCSS]:                 https://github.com/postcss/postcss
+[Assemble]:                http://assemblecss.com
+
+# PostCSS Assemble Button Helper
+
+<img align="right" width="135" height="95"
+     title="Philosopher’s stone, logo of PostCSS"
+     src="http://postcss.github.io/postcss/logo-leftp.png">
+
+[PostCSS] plugin that helps with the use of Assemble Buttons.
+
+This plugin will help set colors of buttons when using the [Assemble] framework.
+
+## Example
+```css
+.btn--primary{
+    bg-color: green;
+    text-color: white;
+    border-color: black;
+    bg-hover-color: orange;
+    text-hover-color: black;
+    border-hover-color: red;
+}
+```
+
+Will output:
+
+```css
+.btn--primary{
+    background: green;
+    color: white;
+    border-color: black;
+}
+
+.btn--primary:hover,
+.btn--primary:active{
+    background: orange;
+    color: black;
+    border-color: red;
+}
+```
+
+## Usage
+
+This plugin doesn't do much on it's own. It is part of a [PostCSS] framework called [Assemble]. This plugin is included in Assemble. For usage see [Assemble].
+
+The options that appear in the example will work in any class that has '.btn--' in it.
---
+++ index.js
@@ -0,0 +1,124 @@
+var postcss = require('postcss');
+
+module.exports = postcss.plugin('assemble-button-helper', function (options) {
+    return function (css) {
+console.log('work!');
+
+        css.walkRules(function (rule) {
+            var notificationRule = rule.selector;
+            var ruleScan = '';
+
+            console.log('hello');
+
+            if (notificationRule.indexOf('.btn--') > -1) {
+
+                rule.walkDecls(function (decl, i) {
+                    var property = decl.prop,
+                        value = decl.value,
+                        origRule = decl.parent,
+                        ruleSelectors = origRule.selectors,
+                        newRule;
+
+                    // Background
+                    if (property == 'bg-color') {
+                        // add background-color styles
+                        decl.cloneBefore({ prop: 'background',  value: value });
+
+                        // Remove the original declaration
+                        decl.remove();
+                    }
+
+                    // Text Color
+                    if (property == 'text-color') {
+                        // add background-color styles
+                        decl.cloneBefore({ prop: 'color',  value: value });
+
+                        // Remove the original declaration
+                        decl.remove();
+                    }
+
+                    // Border Color
+                    if (property == 'border-color') {
+                        // add background-color styles
+                        decl.cloneBefore({ prop: 'border-color',  value: value });
+
+                        // Remove the original declaration
+                        decl.remove();
+                    }
+
+                    // BG Hover Color
+                    if (property == 'bg-hover-color') {
+
+                        if (ruleScan !== '') {
+                            ruleScan.append('background:' + value + ';');
+                        } else {
+                            ruleSelectors = ruleSelectors.map(function(ruleSelector){
+                                return notificationRule + ':hover,\n' + notificationRule + ':active';
+                            }).join(',\n');
+
+                            newRule = origRule.cloneAfter({
+                                selector: ruleSelectors
+                            }).removeAll();
+
+                            newRule.append('background:' + value + ';');
+
+                            ruleScan = newRule;
+
+                        }
+
+                        // Remove the original declaration
+                        decl.remove();
+                    }
+
+
+                    // Text Hover Color
+                    if (property == 'text-hover-color') {
+
+                        if (ruleScan !== '') {
+                            ruleScan.append('color:' + value + ';');
+                        } else {
+                            ruleSelectors = ruleSelectors.map(function(ruleSelector){
+                                return notificationRule + ':hover,\n' + notificationRule + ':active';
+                            }).join(',\n');
+
+                            newRule = origRule.cloneAfter({
+                                selector: ruleSelectors
+                            }).removeAll();
+
+                            newRule.append('color:' + value + ';');
+
+                            ruleScan = newRule;
+                        }
+
+                        // Remove the original declaration
+                        decl.remove();
+                    }
+
+                    // Border Hover Color
+                    if (property == 'border-hover-color') {
+
+                        if (ruleScan !== '') {
+                            ruleScan.append('border-color:' + value + ';');
+                        } else {
+                            ruleSelectors = ruleSelectors.map(function(ruleSelector){
+                                return notificationRule + ':hover,\n' + notificationRule + ':active';
+                            }).join(',\n');
+
+                            newRule = origRule.cloneAfter({
+                                selector: ruleSelectors
+                            }).removeAll();
+
+                            newRule.append('border-color:' + value + ';');
+
+                            ruleScan = newRule;
+                        }
+
+                        // Remove the original declaration
+                        decl.remove();
+                    }
+
+                });
+            }
+        });
+    };
+});
